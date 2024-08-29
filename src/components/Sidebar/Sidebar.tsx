c43053057732a7ade01c68ae6e0b0893058b5a43
Kyrylo Hrecheniuk <kyryloh@wix.com>
2024-08-29 19:32:41 +0200

    sidebar for mobile

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,25 +1,37 @@
 'use client'
 
+import { HamburgerButton } from '@/components/HamburgerButton/HamburgerButton'
 import { HexagonLabel } from '@/components/HexagonLabel/HexagonLabel'
 import { useQuestionsContext } from '@/contexts/QuestionsContext/QuestionsContext'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import s from './Sidebar.module.css'
 
 export const Sidebar: FC = () => {
   const { questions, currentQuestionIndex } = useQuestionsContext()
+  const [isOpen, setIsOpen] = useState(false)
 
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const className = isOpen ? s.containerOpen : s.container
 
   return (
-    <aside className={s.container}>
-      <ul className={s.achievements}>
-        {questions.map((question, index) =>
-          (
-            <li key={question.key}>
-              <HexagonLabel amount={question.amount} isEarned={index < currentQuestionIndex} isCurrent={index === currentQuestionIndex} />
-            </li>
-          ))}
-      </ul>
-    </aside>
+    <>
+      <div className={s.hamburger}>
+        <HamburgerButton isOpen={isOpen} onClick={handleClick} />
+      </div>
+      <aside className={className}>
+        <ul className={s.achievements}>
+          {questions.map((question, index) =>
+            (
+              <li key={question.key}>
+                <HexagonLabel amount={question.amount} isEarned={index < currentQuestionIndex} isCurrent={index === currentQuestionIndex} />
+              </li>
+            ))}
+        </ul>
+      </aside>
+    </>
   )
 }
 
